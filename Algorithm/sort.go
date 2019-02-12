276d0c011d52350db765099111172e6eb3ce8497
zhouletian1234@live.com <zhouletian1234@live.com>
2019-02-12 14:26:54 +0800

    add sort algorithm

---
+++ Algorithm/sort.go
@@ -0,0 +1,48 @@
+package Algorithm
+
+type Sort struct {
+	 Data []int
+	 Interval int64
+}
+
+func (s * Sort)BubbleSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)SelectSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)InsertSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)MergeSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)ShellSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)QuickSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)HeapSort()([]int) {
+
+	return nil
+}
+
+func (s * Sort)RadixSort()([]int) {
+
+	return nil
+}
+
+
