5a44505096401e0638fe659d2ce3fe862c071641
zhouletian1234@live.com <zhouletian1234@live.com>
2019-02-12 14:34:44 +0800

    add heap

---
+++ Algorithm/heap.go
@@ -0,0 +1,20 @@
+package Algorithm
+
+type Heap struct {
+	 Data []int
+	 Size int
+}
+
+func (h *Heap)initHeap() {
+
+}
+
+func (h * Heap)Pop()(int) {
+
+	return 0
+}
+
+func (h * Heap)Insert(d int) {
+
+}
+
